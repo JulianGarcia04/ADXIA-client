716dca446eaf42b4900c780a6c730a4a9a3aa4bb
Julian Garcia <jcgrrincon@gmail.com>
2022-11-30 21:00:50 -0500

    little changes in the personCard

--- components/PersonCard/View.jsx
+++ components/PersonCard/View.jsx
@@ -39,8 +39,8 @@ function View({ options, modalState, changeModalState, idPerson, employee }) {
           "https://scontent.fctg1-4.fna.fbcdn.net/v/t39.30808-6/275576245_368936285098451_4743975802562067660_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_DlDHrxP7a0AX981eE3&_nc_ht=scontent.fctg1-4.fna&oh=00_AfCDdg56sBuCW11f_qHmGK2Qflt3dQ5IbSvysXmWoKuRvg&oe=6379AA04"
         }
         alt="Image profile"
-        width={60}
-        height={60}
+        width={50}
+        height={50}
         style={{ borderRadius: "50%" }}
       />
       <div className={style.infoPerson}>
