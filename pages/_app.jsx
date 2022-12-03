19a02fab9eabe8c57952a99fa47e64b1a879dc71
Julian Garcia <jcgrrincon@gmail.com>
2022-12-02 22:20:50 -0500

    implement the config of react-query

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,17 @@
 import { Provider } from "react-redux";
+import { QueryClientProvider } from "react-query";
+import queryClient from "~/query/queryClient";
 import store from "../redux/store";
 import "../styles/globalStyles.scss";
 
-const MyApp = ({Component, pageProps})=>{
-    return  <Provider store={store}>
-                <Component {...pageProps}/>
-            </Provider>
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
+  );
+};
 
 export default MyApp;
