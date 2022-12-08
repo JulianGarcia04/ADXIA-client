8e63cc0e2bd27f4e726d360ce75a688e8e620d98
Julian Garcia <jcgrrincon@gmail.com>
2022-12-08 11:19:38 -0500

    changes in the index of the project for that redirect to the login view

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
+import { NextResponse } from "next/server";
 
-export default function Index({message}) {
-  return (
-    <div>{message}</div>
-  )
+export default function Index({ message }) {
+  return <div>{message}</div>;
 }
 
 export async function getServerSideProps(context) {
-    return {
-      props: {
-          message: "Hola Mundo"
-      }, // will be passed to the page component as props
-    }
-  }
+
+  return {
+    props: {
+      message: "Hola Mundo",
+    },
+    redirect: {
+      destination: '/login'
+    },
+  };
+}
